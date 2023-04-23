b175664d6457540bc09ca4f5e2e5f473ae42e8dc
Slava Vasiliev <sb-fantom@yandex.ru>
2023-04-23 10:08:01 +0300

    1 change README.md

--- src/main/java/ru/netology/javaqadiplom/SavingAccount.java
+++ src/main/java/ru/netology/javaqadiplom/SavingAccount.java
@@ -3,22 +3,47 @@ package ru.netology.javaqadiplom;
 public class SavingAccount extends Account {
     protected int minBalance;
     protected int maxBalance;
+
     public SavingAccount(int initialBalance, int minBalance, int maxBalance, int rate) {
         if (rate < 0) {
             throw new IllegalArgumentException(
-              "Накопительная ставка не может быть отрицательной, а у вас: " + rate
+                    "Накопительная ставка не может быть отрицательной, а у вас: " + rate
             );
         }
+//        if (minBalance > maxBalance) {
+//            throw new IllegalArgumentException(
+//                    "Минимальный баланс не может быть больше максимального, а у вас: " + minBalance + " > " + maxBalance
+//            );
+//        }
+//        if (initialBalance < minBalance) {
+//            throw new IllegalArgumentException(
+//                    "Начальный баланс не может быть меньше минимального, а у вас: " + initialBalance + " < " + minBalance
+//            );
+//        if (initialBalance > maxBalance) {
+//            throw new IllegalArgumentException(
+//                    "Начальный баланс не может быть больше масимального, а у вас: " + initialBalance + " > " + maxBalance
+//            );
+//        }
+//        if (minBalance  < 0) {
+//            throw new IllegalArgumentException(
+//                    "Минимальный баланс не может быть отрицательным числом, а у вас: " + minBalance + " < " + 0
+//            );
+//        }
+
         this.balance = initialBalance;
         this.minBalance = minBalance;
         this.maxBalance = maxBalance;
         this.rate = rate;
     }
+
     @Override
     public boolean pay(int amount) {
         if (amount <= 0) {
             return false;
         }
+//        if (amount <= balance) {
+//            balance = balance - amount;
+//        }
         balance = balance - amount;
         if (balance > minBalance) {
             return true;
@@ -26,20 +51,24 @@ public class SavingAccount extends Account {
             return false;
         }
     }
+
     @Override
     public boolean add(int amount) {
         if (amount <= 0) {
             return false;
         }
         if (balance + amount < maxBalance) {
+//            balance = amount + balance;
             balance = amount;
             return true;
         } else {
             return false;
         }
     }
+
     @Override
     public int yearChange() {
+//        return balance = balance * rate / 100;
         return balance / 100 * rate;
     }
 
